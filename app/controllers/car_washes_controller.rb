37fc75f1f535c22e79efbb5c45f4fa019046c363
MFitzgerald89 <122069535+MFitzgerald89@users.noreply.github.com>
2023-06-05 20:27:35 -0400

    adds admin, lat, lon, city, state to users table/adds carwash controller

---
+++ app/controllers/car_washes_controller.rb
@@ -0,0 +1,5 @@
+class CarWashesController < ApplicationController
+
+  before_action :authenticate_user
+
+end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,11 @@ class User < ApplicationRecord
   has_secure_password
   validates :email, presence: true, uniqueness: true
 
+  # get user coordinates based on provided city and state input
+  geocoded_by :address
+  after_validation :geocode
+
+  def address
+    [city, state].compact.join(", ")
+  end
 end
