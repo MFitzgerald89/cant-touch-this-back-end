10a5330dfae81c04308971c894d5bd8f1ca8b10f
MFitzgerald89 <122069535+MFitzgerald89@users.noreply.github.com>
2023-06-10 17:54:36 -0400

    adds keywords to reject for car washing data populating

--- app/controllers/car_washes_controller.rb
+++ app/controllers/car_washes_controller.rb
@@ -2,16 +2,20 @@ class CarWashesController < ApplicationController
 
   before_action :authenticate_user
 
-  # show all coffee shops based on user coordinates
+  # show all car_washes based on user coordinates
   def index
     lat = current_user.latitude
     lon = current_user.longitude
     response = HTTP.get("https://maps.googleapis.com/maps/api/place/textsearch/json?query=carwash&location=#{lat},#{lon}&radius=10000&region=us&type=car_wash&key=#{Rails.application.credentials.google_maps_api_key}")
     data = response.parse(:json)
     data = data["results"]
-    bad_wash = ["Mr. Magic Car Wash - Brentwood", "Brushless", "Brush-less", "Brush Less", "Brush-Less", "SoftTouch", "Softtouch", "Soft Touch", "Soft-Touch", "Touch", "Self-Service", "Self Service", "Self-Serve", "Self Serve"]
-    data = data.delete_if { |x| bad_wash.include? x["name"]}
+    # bad_wash = ["Mr. Magic", "Brushless", "Brush-less", "Brush Less", "Brush-Less", "SoftTouch", "Softtouch", "Soft Touch", "Soft-Touch", "Touch", "Self-Service", "Self Service", "Self-Serve", "Self Serve"]
+    # data = data.delete_if { |x| bad_wash.include? x["name"]}
+    # key = "Touchless"
+    excluded_keywords = ["Mr. Magic", "Brushless", "Brush-less", "Brush Less", "Brush-Less", "SoftTouch", "Softtouch", "Soft Touch", "Soft-Touch", "Touch", "Self-Service", "Self Service", "Self-Serve", "Self Serve"]
     @car_washes = data
+    @car_washes = @car_washes.reject { |wash| excluded_keywords.any? { |keyword| wash["name"].include?(keyword) } }
+
     render json: @car_washes
   end
 
