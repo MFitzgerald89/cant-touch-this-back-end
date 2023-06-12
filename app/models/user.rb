db915c8a35553ab5a3f55fba92ed847a7ffea131
MFitzgerald89 <122069535+MFitzgerald89@users.noreply.github.com>
2023-06-12 17:35:51 -0400

    updates car_washes controller

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,6 +6,7 @@ class User < ApplicationRecord
   # get user coordinates based on provided city and state input
   geocoded_by :address
   after_validation :geocode
+  has_many :car_washes
 
   def address
     [city, state].compact.join(", ")
--- app/views/users/_user.json.jbuilder
+++ app/views/users/_user.json.jbuilder
@@ -2,3 +2,7 @@ json.id user.id
 json.first_name user.first_name
 json.last_name user.last_name
 json.email user.email
+json.latitude user.latitude
+json.longitude user.longitude
+json.city user.city
+json.state user.state
